ab66fdbafa9c3a5a89fe3fb792b6c8dc9d5086ae
Helio Brito de Assuncao <84857218+helioBAssuncao@users.noreply.github.com>
2021-08-29 16:28:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 script.js
+< script >
+  function  pergunta1 ( )  {
+  if  ( confirm ( "Deseja enviar agora?" ) )  {
+  return  true ;
+}
+  else  {
+  return  false ;
+}
+}
+
+  function  pergunta2 ( )  {
+  alerta ( “Todos os campos apagados.” ) ;
+  }
+  </ script >
